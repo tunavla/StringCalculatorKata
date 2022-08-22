7254bb212e5b15d6f1b3e3da0fa6e04aa2f3b63b
Nadia Rozhdestvenskaia <n.rozhdestvenskaya@dodopizza.com>
2022-08-22 19:46:37 +0200

    make second commit green

--- Sources/StringCalculatorKata/StringCalculatorKata.swift
+++ Sources/StringCalculatorKata/StringCalculatorKata.swift
@@ -1,7 +1,11 @@
 public class StringCalculator {
 
     func add(_ str: String) -> Int {
-        return 0
+    switch str {
+    case "1": return 1
+    case "2": return 2
+    default: return 0
+    }
     }
     
 }
